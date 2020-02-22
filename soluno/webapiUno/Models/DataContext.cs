27474c702c6f7235e473eee6a38c913571f8ae55
OsmanVelarde <osmastersyco@gmail.com>
2020-02-22 11:30:31 -0400

    creacion de web api completo

---
+++ soluno/webapiUno/Models/DataContext.cs
@@ -0,0 +1,13 @@
+﻿namespace webapiUno.Models
+{
+    using System.Data.Entity;
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<webapiUno.Models.Person> People { get; set; }
+    }
+}
